5dd24b65b097e6c3b7e4bd06c951ff670facb55b
liuweigang <liuwg2015@163.com>
2023-09-20 16:48:52 +0800

    新增消息推送保存

--- utils/file_tool.go
+++ utils/file_tool.go
@@ -1,7 +1,11 @@
 package utils
 
 import (
+	"archive/zip"
+	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 )
 
 func CheckExists(path string) {
@@ -16,6 +20,64 @@ func CheckExists(path string) {
 	return
 }
 
-func MakeDir(path string) {
+func ZipFolder(source, target string) error {
+	// 创建目标压缩文件
+	zipFile, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer zipFile.Close()
 
+	// 创建压缩器
+	zipWriter := zip.NewWriter(zipFile)
+	defer zipWriter.Close()
+
+	// 遍历源文件夹中的所有文件和子文件夹
+	filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// 获取相对路径
+		relPath, err := filepath.Rel(source, path)
+		if err != nil {
+			return err
+		}
+
+		// 创建文件头信息
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		// 设置文件头中的相对路径
+		header.Name = relPath
+
+		// 判断是否为文件夹
+		if info.IsDir() {
+			header.Name += "/"
+		} else {
+			// 添加文件到压缩包中
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+
+			writer, err := zipWriter.CreateHeader(header)
+			if err != nil {
+				return err
+			}
+
+			_, err = io.Copy(writer, file)
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+
+	fmt.Println("压缩完成:", target)
+	return nil
 }
